208cd25c53c95612eea727826dc7d16c3ebcbacb
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-18 17:47:44 +0100

    Added constraints code

--- wiezy.sql
+++ wiezy.sql
@@ -10,3 +10,65 @@ ALTER TABLE CarMark ADD (
     CONSTRAINT carMarkNameUN UNIQUE (carMarkName)
 );
 
+-- Car
+ALTER TABLE Car ADD (
+    CONSTRAINT carPK PRIMARY KEY (carKey)
+);
+
+-- BlaBlaUser
+ALTER TABLE BlaBlaUser ADD (
+    CONSTRAINT blaBlaUserPK PRIMARY KEY (blaBlaUserKey),
+    CONSTRAINT blaBlaUserUsernameUN UNIQUE (username),
+    CONSTRAINT blaBlaUserEmailUN UNIQUE (email),
+);
+
+-- PostStatus
+ALTER TABLE PostStatus ADD (
+    CONSTRAINT postStatusPK PRIMARY KEY (postStatusKey),
+    CONSTRAINT postStatusNameUN UNIQUE (postStatusName)
+);
+
+-- Post
+ALTER TABLE Post ADD (
+    CONSTRAINT postPK PRIMARY KEY (postKey)
+);
+
+-- Address
+ALTER TABLE Address ADD (
+    CONSTRAINT addressPK PRIMARY KEY (addressKey)
+);
+
+-- Country
+ALTER TABLE Country ADD (
+    CONSTRAINT countryPK PRIMARY KEY (countryKey),
+    CONSTRAINT countryNameUN UNIQUE (countryName)
+);
+
+-- City
+ALTER TABLE City ADD (
+    CONSTRAINT cityPK PRIMARY KEY (cityKey),
+    CONSTRAINT cityNameUN UNIQUE (cityName),
+    CONSTRAINT cityCountryFK FOREIGN KEY (countryKey) REFERENCES Country
+);
+
+-- PaymentMethod
+ALTER TABLE PaymentMethod ADD (
+    CONSTRAINT paymentMethodPK PRIMARY KEY (paymentMethodKey),
+    CONSTRAINT paymentMethodNameUN UNIQUE (paymentMethodName)
+);
+
+-- PaymentStatus
+ALTER TABLE PaymentStatus ADD (
+    CONSTRAINT paymentStatusPK PRIMARY KEY (paymentStatusKey),
+    CONSTRAINT paymentStatusNameUN UNIQUE (paymentStatusName)
+);
+
+-- Payment
+ALTER TABLE Payment ADD (
+    CONSTRAINT paymentPK PRIMARY KEY (paymentKey)
+);
+
+-- Travel
+ALTER TABLE Travel ADD (
+    CONSTRAINT travelPK PRIMARY KEY (travelKey, blaBlaUserKey)
+);
