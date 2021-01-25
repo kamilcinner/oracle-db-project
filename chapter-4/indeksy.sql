494c3d6df2c23e7d021b96bf9c2b064546eb7eff
Magdalena Lenart <magdalenart333@gmail.com>
2021-01-25 23:37:36 +0100

    Changed

--- chapter-4/indeksy.sql
+++ chapter-4/indeksy.sql
@@ -13,8 +13,8 @@ CREATE INDEX carColor_carColorType_IDX ON carColor (carColorTypeKey);
 -- Car
 CREATE INDEX car_carMark_IDX ON Car (carMarkKey);
 CREATE INDEX car_carColor_IDX ON Car (carColorKey);
-CREATE INDEX car_carTypeKey_IDX ON Car (carTypeKey);
-CREATE INDEX car_userKey_IDX ON Car (userKey);
+CREATE INDEX car_carType_IDX ON Car (carTypeKey);
+CREATE INDEX car_user_IDX ON Car (userKey);
 
 -- User
 CREATE INDEX user_firstnameSurname_IDX ON "USER" (firstname, surname);
@@ -35,12 +35,12 @@ CREATE INDEX userStatusHistory_user_IDX ON UserStatusHistory (userKey);
 
 
 -- Address
-CREATE INDEX address_cityKey_IDX ON Address (cityKey);
+CREATE INDEX address_city_IDX ON Address (cityKey);
 CREATE INDEX address_postCode_IDX ON Address (postCode);
 CREATE INDEX address_street_IDX ON Address (street);
 
 -- City
-CREATE INDEX city_countryKey_IDX ON City (countryKey);
+CREATE INDEX city_country_IDX ON City (countryKey);
 
 -- Country
 
