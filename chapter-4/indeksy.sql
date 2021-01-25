7f30c2d7e14981e04da6d6141296645396414b5c
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-25 16:41:58 +0100

    Added indexes file schema

--- chapter-4/indeksy.sql
+++ chapter-4/indeksy.sql
@@ -1,9 +1,60 @@
--- Post
-CREATE INDEX post_postStatusKey_IDX ON Post(postStatusKey);
+-- CarColor
 
--- User
-CREATE INDEX user_firstnameSurname_IDX ON "USER"(firstname, surname);
+
+-- CarColorType
+
+
+-- CarMark
+
+
+-- CarType
+
 
 -- Car
 CREATE INDEX car_markKey_IDX ON Car(carMarkKey);
 CREATE INDEX car_colorKey_IDX ON Car(carColorKey);
+
+-- User
+CREATE INDEX user_firstnameSurname_IDX ON "USER"(firstname, surname);
+
+-- UserStatus
+
+
+-- UserStatusHistory
+
+
+-- PostStatus
+
+
+-- PostStatusHistory
+
+
+-- Post
+CREATE INDEX post_postStatusKey_IDX ON Post(postStatusKey);
+
+-- Address
+
+
+-- City
+
+
+-- Country
+
+
+-- PaymentMethod
+
+
+-- PaymentStatus
+
+
+-- Payment
+
+
+-- ReservationStatus
+
+
+-- ReservationStatusHistory
+
+
+-- Reservation
+
