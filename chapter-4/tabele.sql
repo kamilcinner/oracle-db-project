b85d887ac60a617870c43fb388c3f42a7e66a069
Kamil Cinner <kamil.cinner@gmail.com>
2021-03-22 09:10:13 +0100

    Adapted create table script to my-sql environment

--- chapter-4/tabele.sql
+++ chapter-4/tabele.sql
@@ -1,44 +1,41 @@
+#USE blablacar;
+#SET sql_mode='ANSI_QUOTES';
+
 -- CarColor
 CREATE TABLE CarColor (
-    carColorKey NUMBER(4),
+    carColorKey INT,
     carColorName VARCHAR(40) NOT NULL,
-    carColorTypeKey NUMBER(4) NOT NULL
-);
-
--- CarColorType
-CREATE TABLE CarColorType (
-    carColorTypeKey NUMBER(4),
-    carColorTypeName VARCHAR(40) NOT NULL
+    carColorTypeKey INT NOT NULL
 );
 
 -- CarMark
 CREATE TABLE CarMark (
-    carMarkKey NUMBER(4),
+    carMarkKey INT,
     carMarkName VARCHAR(40) NOT NULL
 );
 
 -- CarType
 CREATE TABLE CarType(
-    carTypeKey NUMBER(4),
+    carTypeKey INT,
     carTypeName VARCHAR(20) NOT NULL
 );
 
 -- Car
 CREATE TABLE Car (
-    carKey NUMBER(10),
+    carKey INT,
     regNumber VARCHAR(20) NOT NULL,
     prodYear CHAR(4) NOT NULL,
-    seatsCount NUMBER(10) NOT NULL,
-    userKey NUMBER(10) NOT NULL,
-    carMarkKey NUMBER(4) NOT NULL,
+    seatsCount INT NOT NULL,
+    userKey INT NOT NULL,
+    carMarkKey INT NOT NULL,
     model VARCHAR(30) NOT NULL,
-    carColorKey NUMBER(4) NOT NULL,
-    carTypeKey NUMBER(4) NOT NULL
+    carColorKey INT NOT NULL,
+    carTypeKey INT NOT NULL
 );
 
 -- User
-CREATE TABLE "USER" (
-    userKey NUMBER(10),
+CREATE TABLE `User` (
+    userKey INT,
     username VARCHAR(20) NOT NULL,
     hashedPassword VARCHAR(100) NOT NULL,
     salt VARCHAR(100) NOT NULL,
@@ -48,134 +45,134 @@ CREATE TABLE "USER" (
     surname VARCHAR(20) NOT NULL,
     dateOfBirth DATE NOT NULL,
     phoneNumber VARCHAR(9) NOT NULL,
-    addressKey NUMBER(10)
+    addressKey INT
 );
 
 -- UserStatus
 CREATE TABLE UserStatus (
-    userStatusKey NUMBER(4),
+    userStatusKey INT,
     userStatusName VARCHAR(20) NOT NULL,
     userStatusDescription VARCHAR(100)
 );
 
 -- UserStatusHistory
 CREATE TABLE UserStatusHistory (
-    userStatusHistoryKey NUMBER(4),
+    userStatusHistoryKey INT,
     changeDateTime TIMESTAMP NOT NULL,
-    userKey NUMBER(10) NOT NULL,
-    userStatusKey NUMBER(4) NOT NULL
+    userKey INT NOT NULL,
+    userStatusKey INT NOT NULL
 );
 
 -- PostStatus
 CREATE TABLE PostStatus (
-    postStatusKey NUMBER(4),
+    postStatusKey INT,
     postStatusName VARCHAR(20) NOT NULL
 );
 
 -- PostStatusHistory
 CREATE TABLE PostStatusHistory (
-    postStatusHistoryKey NUMBER(4),
+    postStatusHistoryKey INT,
     changeDateTime TIMESTAMP NOT NULL,
-    postKey NUMBER(10) NOT NULL,
-    postStatusKey NUMBER(4) NOT NULL
+    postKey INT NOT NULL,
+    postStatusKey INT NOT NULL
 );
 
 -- Post
 CREATE TABLE Post (
-    postKey NUMBER(10),
+    postKey INT,
     departureDateTime TIMESTAMP NOT NULL,
     arrivalDateTime TIMESTAMP NOT NULL,
-    seatsCount NUMBER(10) NOT NULL,
-    seatPrice NUMBER(10, 2) NOT NULL,
-    departureAddressKey NUMBER(10) NOT NULL,
-    arrivalAddressKey NUMBER(10) NOT NULL,
-    carKey NUMBER(10) NOT NULL
+    seatsCount INT NOT NULL,
+    seatPrice DECIMAL(10, 2) NOT NULL,
+    departureAddressKey INT NOT NULL,
+    arrivalAddressKey INT NOT NULL,
+    carKey INT NOT NULL
 );
 
 -- Address
 CREATE TABLE Address (
-    addressKey NUMBER(10),
+    addressKey INT,
     postCode VARCHAR(5) NOT NULL,
     street VARCHAR(20) NOT NULL,
     houseNumber VARCHAR(20) NOT NULL,
     flatNumber VARCHAR(20),
-    cityKey NUMBER(10) NOT NULL
+    cityKey INT NOT NULL
 );
 
 -- City
 CREATE TABLE City (
-    cityKey NUMBER(10),
+    cityKey INT,
     cityName VARCHAR(20) NOT NULL,
-    countryKey NUMBER(10) NOT NULL
+    countryKey INT NOT NULL
 );
 
 -- Country
 CREATE TABLE Country (
-    countryKey NUMBER(10),
+    countryKey INT,
     countryName VARCHAR(20) NOT NULL
 );
 
 -- PaymentMethod
 CREATE TABLE PaymentMethod (
-    paymentMethodKey NUMBER(4),
-    paymentMethodName VARCHAR2(20) NOT NULL
+    paymentMethodKey INT,
+    paymentMethodName VARCHAR(20) NOT NULL
 );
 
 -- PaymentStatus
 CREATE TABLE PaymentStatus (
-    paymentStatusKey NUMBER(4),
-    paymentStatusName VARCHAR2(20) NOT NULL
+    paymentStatusKey INT,
+    paymentStatusName VARCHAR(20) NOT NULL
 );
 
 -- PaymentStatusHistory
 CREATE TABLE PaymentStatusHistory (
-    paymentStatusHistoryKey NUMBER(10),
+    paymentStatusHistoryKey INT,
     changeDateTime TIMESTAMP NOT NULL,
-    paymentKey NUMBER(10) NOT NULL,
-    paymentStatusKey NUMBER(4) NOT NULL
+    paymentKey INT NOT NULL,
+    paymentStatusKey INT NOT NULL
 );
 
 -- Payment
 CREATE TABLE Payment (
-    paymentKey NUMBER(10),
-    totalAmount NUMBER(10, 2) NOT NULL,
+    paymentKey INT,
+    totalAmount DECIMAL(10, 2) NOT NULL,
     externalPaymentNumber VARCHAR(40) NOT NULL,
-    paymentMethodKey NUMBER(4) NOT NULL,
-    reservationKey NUMBER(10) NOT NULL
+    paymentMethodKey INT NOT NULL,
+    reservationKey INT NOT NULL
 );
 
 -- ReservationStatus
 CREATE TABLE ReservationStatus (
-    reservationStatusKey NUMBER(4),
+    reservationStatusKey INT,
     reservationStatusName VARCHAR(20) NOT NULL
 );
 
 -- ReservationStatusHistory
 CREATE TABLE ReservationStatusHistory (
-    reservationStatusHistoryKey NUMBER(4),
+    reservationStatusHistoryKey INT,
     changeDateTime TIMESTAMP NOT NULL,
-    reservationKey NUMBER(10) NOT NULL,
-    reservationStatusKey NUMBER(4) NOT NULL
+    reservationKey INT NOT NULL,
+    reservationStatusKey INT NOT NULL
 );
 
 -- Reservation
 CREATE TABLE Reservation (
-    reservationKey NUMBER(10),
+    reservationKey INT,
     reservationDateTime TIMESTAMP NOT NULL,
-    seatsCount NUMBER(10) NOT NULL,
-    postKey NUMBER(10) NOT NULL,
-    userKey NUMBER(10) NOT NULL
+    seatsCount INT NOT NULL,
+    postKey INT NOT NULL,
+    userKey INT NOT NULL
 );
 
 -- Role
-CREATE TABLE "ROLE" (
-    roleKey NUMBER(4),
+CREATE TABLE `Role` (
+    roleKey INT,
     roleName VARCHAR(20) NOT NULL
 );
 
 -- UserRole
 CREATE TABLE UserRole (
-    userRoleKey NUMBER(10),
-    userKey NUMBER(10) NOT NULL,
-    roleKey NUMBER(4) NOT NULL
+    userRoleKey INT,
+    userKey INT NOT NULL,
+    roleKey INT NOT NULL
 );
